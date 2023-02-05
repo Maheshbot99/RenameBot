4ffc97ef220a7d25ae66d5ed577888de68ced4ba
Maheshbot99 <115773959+Maheshbot99@users.noreply.github.com>
2023-02-06 00:22:25 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -4,7 +4,7 @@ from pyrogram import Client , filters
 
 @Client.on_callback_query(filters.regex('upgrade'))
 async def upgrade(bot,update):
-	text = """**Free Plan User**
+	text = """ **Free Plan User**
 	**Daily Upload Limit 4GB**
 	**Price :- 0**
 	
@@ -34,7 +34,7 @@ async def upgrade(bot,update):
 
 @Client.on_message(filters.private & filters.command(["upgrade"]))
 async def upgradecm(bot,message):
-	text = """**Free Plan User**
+	text = """ **Free Plan User**
 	**Daily Upload Limit 4GB**
 	**Price :- 0**
 	
