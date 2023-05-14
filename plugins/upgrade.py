a89ef1faaca9da8ce7796be6898cfc82e1645191
Maheshbot99 <115773959+Maheshbot99@users.noreply.github.com>
2023-05-14 13:41:16 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -20,7 +20,13 @@ async def upgrade(bot,update):
 
 @Client.on_message(filters.private & filters.command(["upgrade"]))
 async def upgradecm(bot,message):
-	text = """ """
+	text = """ **🏷 ᴄᴜʀʀᴇɴᴛ ᴘʟᴀɴ** :- Free
+
+**⌾ ᴅᴀɪʟʏ ᴜᴘʟᴏᴀᴅ** :- 4.01 GB
+**⌾ ᴛɪᴍᴇ ɢᴀᴘ** :- 1 min
+**⌾ 4ɢʙ sᴜᴘᴘᴏʀᴛ** :- False
+**⌾ ᴘᴀʀᴀʟʟᴇʟ ᴘʀᴏᴄᴇss** :- 1 
+**⌾ ᴠᴀʟɪᴅɪᴛʏ** :- Life Time"""
 	keybord = InlineKeyboardMarkup([[ 
         			InlineKeyboardButton("Cancel",callback_data = "cancel")], 
         			[InlineKeyboardButton("Cancel",callback_data = "cancel"),
