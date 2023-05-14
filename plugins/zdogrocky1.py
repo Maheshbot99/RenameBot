f68c3e771a6678414a547c91053aef52ae65390e
Maheshbot99 <115773959+Maheshbot99@users.noreply.github.com>
2023-05-14 12:50:34 +0530

    Add files via upload

---
+++ plugins/zdogrocky1.py
@@ -0,0 +1,70 @@
+"""lokaman"""
+from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
+from pyrogram import Client , filters
+
+@Client.on_callback_query(filters.regex('upgrade'))
+async def upgrade(bot,update):
+	text = """ **Free Plan User**
+	**Daily Upload Limit 4GB**
+	**4GB File Support :- No**
+        **🤑Price :- 0**
+	
+	**🥈 Silver Plan User 🥈** 
+	**Daily Upload Limit 10GB**
+	**4GB File Support :- Yes**
+	**🤑Price :- Rs 100 ind / 1.22$ Per Month**
+	
+	**🏅 Gold Plan User 🏅**
+	**Daily Upload Limit 50GB**
+	**4GB File Support :- Yes**
+	**🤑Price :- Rs 150 ind / 1.2$ Per Month**
+	
+	**💎 Diamond Plan User 💎**
+	**Daily Upload Limit 100GB**
+	**4GB File Support :- Yes**
+	**🤑Price :- Rs 150 ind / 1.83$ Per Month**
+	
+	
+	**Payment :-** `maheshs458@ybl`
+	
+	**After Payment Send Screenshots Of  
+        Payment To Admin @MaHi_458BOT**"""
+	keybord = InlineKeyboardMarkup([[ 
+        			InlineKeyboardButton("👤 MAHESH 👤",url = "https://t.me/MaHi_458Bot")], 
+        			[InlineKeyboardButton("🎫  PAYTM  🎫",url = "https://p.paytm.me/xCTH/6pd91cj8"),
+        			InlineKeyboardButton('🏠 Your Plan 🏠', callback_data='myplan')],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+	await update.message.edit(text = text,reply_markup = keybord)
+	
+
+@Client.on_message(filters.private & filters.command(["upgrade"]))
+async def upgradecm(bot,message):
+	text = """ **Free Plan User**
+	**Daily Upload Limit 4GB**
+	**4GB File Support :- No**
+	**🤑Price :- 0**
+	
+	**🥈 Silver Plan User 🥈** 
+	**Daily Upload Limit 10GB**
+	**4GB File Support :- Yes**
+	**🤑Price :- Rs 50 ind / 0.6$ Per Month**
+	
+	**🏅 Gold Plan User 🏅**
+	**Daily Upload Limit 50GB**
+	**4GB File Support :- Yes**
+	**🤑Price :- Rs 100 ind / 1.2$ Per Month**
+	
+	**💎 Diamond Plan User 💎**
+	**Daily Upload Limit 100GB**
+	**4GB File Support :- Yes**
+	**🤑Price :- Rs 150 ind / 1.8$ Per Month**
+	
+	
+	**Payment :-** `maheshs458@ybl`
+	
+	**After Payment Send Screenshots Of  
+        Payment To Admin @MaHi_458BOT**"""
+	keybord = InlineKeyboardMarkup([[ 
+        			InlineKeyboardButton("👤  MAHESH  👤",url = "https://t.me/MaHi_458Bot")], 
+        			[InlineKeyboardButton("🎫  PAYTM  🎫",url = "https://p.paytm.me/xCTH/6pd91cj8"),
+        			InlineKeyboardButton('🏠 Your Plan 🏠', callback_data='myplan')],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+	await message.reply_text(text = text,reply_markup = keybord)
