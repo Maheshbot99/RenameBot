50b18f7a0c3182ad949c314fcd5e0357377dc497
Maheshbot99 <115773959+Maheshbot99@users.noreply.github.com>
2023-05-14 13:53:53 +0530

    Update zdogrocky.py

--- plugins/zdogrocky.py
+++ plugins/zdogrocky.py
@@ -2,19 +2,19 @@
 from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 from pyrogram import Client , filters
 
-@Client.on_callback_query(filters.regex('upgrade'))
+@Client.on_callback_query(filters.regex('zdogrocky'))
 async def upgrade(bot,update):
 	text = """ """
 	keybord = InlineKeyboardMarkup([[ 
         			InlineKeyboardButton("👤 MAHESH 👤",url = "https://t.me/MaHi_458Bot")], 
-        			[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+        			[InlineKeyboardButton("• ʙᴀᴄᴋ •",callback_data = "upgrade")  ]])
 	await update.message.edit(text = text,reply_markup = keybord)
 	
 
-@Client.on_message(filters.private & filters.command(["upgrade"]))
+@Client.on_message(filters.private & filters.command(["zdogrocky"]))
 async def upgradecm(bot,message):
 	text = """ """
 	keybord = InlineKeyboardMarkup([[ 
         			InlineKeyboardButton("👤  MAHESH  👤",url = "https://t.me/MaHi_458Bot")], 
-        			[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+        			[InlineKeyboardButton("• ʙᴀᴄᴋ •",callback_data = "upgrade")  ]])
 	await message.reply_text(text = text,reply_markup = keybord)
