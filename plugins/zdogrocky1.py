8797e6725ce7a6f71f6f9cb468e17fd6c287072d
Maheshbot99 <115773959+Maheshbot99@users.noreply.github.com>
2023-05-14 13:59:22 +0530

    Update zdogrocky1.py

--- plugins/zdogrocky1.py
+++ plugins/zdogrocky1.py
@@ -2,19 +2,19 @@
 from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 from pyrogram import Client , filters
 
-@Client.on_callback_query(filters.regex('upgrade'))
+@Client.on_callback_query(filters.regex('zdogrocky1'))
 async def upgrade(bot,update):
 	text = """ """
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("👤 MAHESH 👤",url = "https://t.me/MaHi_458Bot")], 
-        			[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+        			InlineKeyboardButton("💳  ᴜᴩɢʀᴀᴅᴇ",url = "https://graph.org/Buy-05-14")], 
+        			[InlineKeyboardButton("• ʙᴀᴄᴋ •",callback_data = "upgrade")  ]])
 	await update.message.edit(text = text,reply_markup = keybord)
 	
 
-@Client.on_message(filters.private & filters.command(["upgrade"]))
+@Client.on_message(filters.private & filters.command(["zdogrocky1"]))
 async def upgradecm(bot,message):
 	text = """ """
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("👤  MAHESH  👤",url = "https://t.me/MaHi_458Bot")], 
-        			[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+        			InlineKeyboardButton("💳  ᴜᴩɢʀᴀᴅᴇ",url = "https://graph.org/Buy-05-14")], 
+        			[InlineKeyboardButton("• ʙᴀᴄᴋ •",callback_data = "upgrade")  ]])
 	await message.reply_text(text = text,reply_markup = keybord)
