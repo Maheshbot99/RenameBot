1125ed7091607fd64163a4475abfbd33c3517e73
Maheshbot99 <115773959+Maheshbot99@users.noreply.github.com>
2023-05-14 13:12:24 +0530

    Update zdogrocky.py

--- plugins/zdogrocky.py
+++ plugins/zdogrocky.py
@@ -4,31 +4,7 @@ from pyrogram import Client , filters
 
 @Client.on_callback_query(filters.regex('upgrade'))
 async def upgrade(bot,update):
-	text = """ **Free Plan User**
-	**Daily Upload Limit 4GB**
-	**4GB File Support :- No**
-        **🤑Price :- 0**
-	
-	**🥈 Silver Plan User 🥈** 
-	**Daily Upload Limit 10GB**
-	**4GB File Support :- Yes**
-	**🤑Price :- Rs 100 ind / 1.22$ Per Month**
-	
-	**🏅 Gold Plan User 🏅**
-	**Daily Upload Limit 50GB**
-	**4GB File Support :- Yes**
-	**🤑Price :- Rs 150 ind / 1.2$ Per Month**
-	
-	**💎 Diamond Plan User 💎**
-	**Daily Upload Limit 100GB**
-	**4GB File Support :- Yes**
-	**🤑Price :- Rs 150 ind / 1.83$ Per Month**
-	
-	
-	**Payment :-** `maheshs458@ybl`
-	
-	**After Payment Send Screenshots Of  
-        Payment To Admin @MaHi_458BOT**"""
+	text = """ """
 	keybord = InlineKeyboardMarkup([[ 
         			InlineKeyboardButton("👤 MAHESH 👤",url = "https://t.me/MaHi_458Bot")], 
         			[InlineKeyboardButton("🎫  PAYTM  🎫",url = "https://p.paytm.me/xCTH/6pd91cj8"),
@@ -38,31 +14,7 @@ async def upgrade(bot,update):
 
 @Client.on_message(filters.private & filters.command(["upgrade"]))
 async def upgradecm(bot,message):
-	text = """ **Free Plan User**
-	**Daily Upload Limit 4GB**
-	**4GB File Support :- No**
-	**🤑Price :- 0**
-	
-	**🥈 Silver Plan User 🥈** 
-	**Daily Upload Limit 10GB**
-	**4GB File Support :- Yes**
-	**🤑Price :- Rs 50 ind / 0.6$ Per Month**
-	
-	**🏅 Gold Plan User 🏅**
-	**Daily Upload Limit 50GB**
-	**4GB File Support :- Yes**
-	**🤑Price :- Rs 100 ind / 1.2$ Per Month**
-	
-	**💎 Diamond Plan User 💎**
-	**Daily Upload Limit 100GB**
-	**4GB File Support :- Yes**
-	**🤑Price :- Rs 150 ind / 1.8$ Per Month**
-	
-	
-	**Payment :-** `maheshs458@ybl`
-	
-	**After Payment Send Screenshots Of  
-        Payment To Admin @MaHi_458BOT**"""
+	text = """ """
 	keybord = InlineKeyboardMarkup([[ 
         			InlineKeyboardButton("👤  MAHESH  👤",url = "https://t.me/MaHi_458Bot")], 
         			[InlineKeyboardButton("🎫  PAYTM  🎫",url = "https://p.paytm.me/xCTH/6pd91cj8"),
