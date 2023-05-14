3808bb22d6fcf0631e307e4eb02de0b4d26f02a8
Maheshbot99 <115773959+Maheshbot99@users.noreply.github.com>
2023-05-14 20:27:53 +0530

    Update progress.py

--- helper/progress.py
+++ helper/progress.py
@@ -39,7 +39,7 @@ async def progress_for_pyrogram(
             await message.edit(
                 text="{}\n\n{}".format(ud_type, tmp),               
                 reply_markup=InlineKeyboardMarkup( [[
-                    InlineKeyboardButton("✖️ 𝙲𝙰𝙽𝙲𝙴𝙻 ✖️", callback_data="cancel")
+                    InlineKeyboardButton("✖️ ᴄᴀɴᴄᴇʟ", callback_data="cancel")
                     ]]
                 )
             )
