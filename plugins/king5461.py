3b57b9465e1e55a5cae5505f28977a25eb5a5586
Maheshbot99 <115773959+Maheshbot99@users.noreply.github.com>
2023-05-14 14:35:42 +0530

    Update and rename zdogrocky2.py to king5461.py

--- plugins/king5461.py
+++ plugins/king5461.py
@@ -2,7 +2,7 @@
 from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 from pyrogram import Client , filters
 
-@Client.on_callback_query(filters.regex('zdogrocky'))
+@Client.on_callback_query(filters.regex('king5461'))
 async def upgrade(bot,update):
 	text = """ **🏷 ᴘʟᴀɴ** :- Diamond 🥈
 
@@ -19,7 +19,7 @@ async def upgrade(bot,update):
 	await update.message.edit(text = text,reply_markup = keybord)
 	
 
-@Client.on_message(filters.private & filters.command(["zdogrocky"]))
+@Client.on_message(filters.private & filters.command(["king5461"]))
 async def upgradecm(bot,message):
 	text = """ **🏷 ᴘʟᴀɴ** :- Diamond 🥈
 
