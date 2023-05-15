5dc8141aec221d072613ad1642a2e9b06457d94e
Maheshbot99 <115773959+Maheshbot99@users.noreply.github.com>
2023-05-15 14:09:15 +0530

    Update thumbset.py

--- plugins/thumbset.py
+++ plugins/thumbset.py
@@ -4,23 +4,27 @@ from pyrogram import Client , filters
 
 @Client.on_callback_query(filters.regex('kingmahip'))
 async def upgrade(bot,update):
-	text = """ **⚙️ ꜱᴇᴛᴛɪɴɢꜱ**
+	text = """ **🖼️  ᴄᴜꜱᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ**
 
-**ꜱᴇᴛ ᴛʜᴜᴍʙɴᴀɪʟ ꜱᴇᴛᴛɪɴɢꜱ ᴀꜱ yᴏᴜʀ ᴡɪꜱʜ**"""
+**⦿  yᴏᴜ ᴄᴀɴ ᴀᴅᴅ ᴄᴜꜱᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ ꜱɪᴍᴩʟy ʙy ꜱᴇɴᴅɪɴɢ ᴀ ᴩʜᴏᴛᴏ ᴛᴏ ᴍᴇ....**
+
+**⦿ /viewthumb - ᴜꜱᴇ ᴛʜɪꜱ ᴄᴏᴍᴍᴀɴᴅ ᴛᴏ ꜱᴇᴇ yᴏᴜʀ ᴛʜᴜᴍʙɴᴀɪʟ**
+**⦿ /delthumb - ᴜꜱᴇ ᴛʜɪꜱ ᴄᴀᴍᴍᴏɴᴅ ᴛᴏ ᴅᴇʟᴇᴛᴇ yᴏᴜʀ ᴛʜᴜᴍʙɴᴀɪʟ**"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("ꜱᴇᴛ ᴛʜᴜᴍʙɴᴀɪʟ",callback_data = "setthumb")], 
-        			[InlineKeyboardButton("ꜱʜᴏᴡ ᴛʜᴜᴍʙɴᴀɪʟ",callback_data = "viewthumb"),
-        			InlineKeyboardButton("ᴅᴇʟᴇᴛᴇ ᴛʜᴜᴍʙɴᴀɪʟ",callback_data = "delthumb")],[InlineKeyboardButton("• ʙᴀᴄᴋ •",callback_data = "settings")  ]])
+        			InlineKeyboardButton("💳 ᴜᴩɢʀᴀᴅᴇ",callback_data = "upgrade")], 
+        			[InlineKeyboardButton("• ʙᴀᴄᴋ •",callback_data = "settings")  ]])
 	await update.message.edit(text = text,reply_markup = keybord)
 	
 
 @Client.on_message(filters.private & filters.command(["kingmahip"]))
 async def upgradecm(bot,message):
-	text = """ **⚙️ ꜱᴇᴛᴛɪɴɢꜱ**
+	text = """ **🖼️  ᴄᴜꜱᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ**
 
-**ꜱᴇᴛ ᴛʜᴜᴍʙɴᴀɪʟ ꜱᴇᴛᴛɪɴɢꜱ ᴀꜱ yᴏᴜʀ ᴡɪꜱʜ**"""
+**⦿  yᴏᴜ ᴄᴀɴ ᴀᴅᴅ ᴄᴜꜱᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ ꜱɪᴍᴩʟy ʙy ꜱᴇɴᴅɪɴɢ ᴀ ᴩʜᴏᴛᴏ ᴛᴏ ᴍᴇ....**
+
+**⦿ /viewthumb - ᴜꜱᴇ ᴛʜɪꜱ ᴄᴏᴍᴍᴀɴᴅ ᴛᴏ ꜱᴇᴇ yᴏᴜʀ ᴛʜᴜᴍʙɴᴀɪʟ**
+**⦿ /delthumb - ᴜꜱᴇ ᴛʜɪꜱ ᴄᴀᴍᴍᴏɴᴅ ᴛᴏ ᴅᴇʟᴇᴛᴇ yᴏᴜʀ ᴛʜᴜᴍʙɴᴀɪʟ**"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("ꜱᴇᴛ ᴛʜᴜᴍʙɴᴀɪʟ",callback_data = "setthumb")], 
-        			[InlineKeyboardButton("ꜱʜᴏᴡ ᴛʜᴜᴍʙɴᴀɪʟ",callback_data = "viewthumb"),
-        			InlineKeyboardButton("ᴅᴇʟᴇᴛᴇ ᴛʜᴜᴍʙɴᴀɪʟ",callback_data = "delthumb")],[InlineKeyboardButton("• ʙᴀᴄᴋ •",callback_data = "settings")  ]])
+        			InlineKeyboardButton("💳 ᴜᴩɢʀᴀᴅᴇ",callback_data = "upgrade")], 
+        			[InlineKeyboardButton("• ʙᴀᴄᴋ •",callback_data = "settings")  ]])
 	await message.reply_text(text = text,reply_markup = keybord)
