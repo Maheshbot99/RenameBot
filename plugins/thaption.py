6fdb510fdae50d4c4f3c14b560047d7bc41f28d2
Maheshbot99 <115773959+Maheshbot99@users.noreply.github.com>
2023-05-15 13:44:27 +0530

    Update thaption.py

--- plugins/thaption.py
+++ plugins/thaption.py
@@ -2,25 +2,25 @@
 from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 from pyrogram import Client , filters
 
-@Client.on_callback_query(filters.regex('settings'))
+@Client.on_callback_query(filters.regex('kingppt'))
 async def upgrade(bot,update):
 	text = """ **⚙️ ꜱᴇᴛᴛɪɴɢꜱ**
 
 **ꜱᴇᴛ ᴄᴀᴩᴛɪᴏɴ ꜱᴇᴛᴛɪɴɢꜱ ᴀꜱ yᴏᴜʀ ᴡɪꜱʜ**"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("ꜱᴇᴛ ᴄᴀᴩᴛɪᴏɴ",callback_data = "upgrade")], 
-        			[InlineKeyboardButton("ꜱʜᴏᴡ ᴄᴀᴩᴛɪᴏɴ",callback_data = "upgrade"),
-        			InlineKeyboardButton("ᴅᴇʟᴇᴛᴇ ᴄᴀᴩᴛɪᴏɴ",callback_data = "upgrade")],[InlineKeyboardButton("• ʙᴀᴄᴋ •",callback_data = "settings")  ]])
+        			InlineKeyboardButton("ꜱᴇᴛ ᴄᴀᴩᴛɪᴏɴ",callback_data = "set_caption")], 
+        			[InlineKeyboardButton("ꜱʜᴏᴡ ᴄᴀᴩᴛɪᴏɴ",callback_data = "see_caption"),
+        			InlineKeyboardButton("ᴅᴇʟᴇᴛᴇ ᴄᴀᴩᴛɪᴏɴ",callback_data = "del_caption")],[InlineKeyboardButton("• ʙᴀᴄᴋ •",callback_data = "settings")  ]])
 	await update.message.edit(text = text,reply_markup = keybord)
 	
 
-@Client.on_message(filters.private & filters.command(["settings"]))
+@Client.on_message(filters.private & filters.command(["kingppt"]))
 async def upgradecm(bot,message):
 	text = """ **⚙️ ꜱᴇᴛᴛɪɴɢꜱ**
 
 **ꜱᴇᴛ ᴄᴀᴩᴛɪᴏɴ ꜱᴇᴛᴛɪɴɢꜱ ᴀꜱ yᴏᴜʀ ᴡɪꜱʜ**"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("ꜱᴇᴛ ᴄᴀᴩᴛɪᴏɴ",callback_data = "upgrade")], 
-        			[InlineKeyboardButton("ꜱʜᴏᴡ ᴄᴀᴩᴛɪᴏɴ",callback_data = "upgrade"),
-        			InlineKeyboardButton("ᴅᴇʟᴇᴛᴇ ᴄᴀᴩᴛɪᴏɴ",callback_data = "upgrade")],[InlineKeyboardButton("• ʙᴀᴄᴋ •",callback_data = "settings")  ]])
+        			InlineKeyboardButton("ꜱᴇᴛ ᴄᴀᴩᴛɪᴏɴ",callback_data = "set_caption")], 
+        			[InlineKeyboardButton("ꜱʜᴏᴡ ᴄᴀᴩᴛɪᴏɴ",callback_data = "see_caption"),
+        			InlineKeyboardButton("ᴅᴇʟᴇᴛᴇ ᴄᴀᴩᴛɪᴏɴ",callback_data = "del_caption")],[InlineKeyboardButton("• ʙᴀᴄᴋ •",callback_data = "settings")  ]])
 	await message.reply_text(text = text,reply_markup = keybord)
