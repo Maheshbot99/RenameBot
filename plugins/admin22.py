bc7184d78f8fff1d961657dcd692303543a7986b
Maheshbot99 <115773959+Maheshbot99@users.noreply.github.com>
2023-05-20 11:00:58 +0530

    Update admin22.py

--- plugins/admin22.py
+++ plugins/admin22.py
@@ -7,14 +7,10 @@ from helper.database import uploadlimit, usertype, addpre
 ADMIN = int(os.environ.get("ADMIN", 1484670284))
 
 
-@Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["sss"]))
+@Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["adminipl"]))
 async def buypremium(bot, message):
-	await message.reply_text("⊚ ꜱᴇʟᴇᴄᴛ ᴩʟᴀɴ ᴛᴏ ᴜᴩɢʀᴀᴅᴇ...", quote=True, reply_markup=InlineKeyboardMarkup([
+	await message.reply_text("**📚 ᴀᴅᴍɪɴ ᴄᴏᴍᴍᴀɴᴅꜱ : \n➢ /users - ᴛᴏ ᴠɪᴇᴡ ʟɪꜱᴛ ᴏꜰ ᴜꜱᴇʀꜱ \n➢ /broadcast - ᴍᴇꜱꜱᴀɢᴇ ʙʀᴏᴀᴅᴄᴀꜱᴛ \n➢ /ceasepower - ᴛᴏ ᴄᴇᴀꜱᴇ (ᴅᴏᴡɴɢʀᴀᴅᴇ) ʀᴇɴᴀᴍɪɴɢ ᴄᴀᴩᴀᴄɪᴛy \n➢ /resetpower - ᴛᴏ ʀᴇꜱᴇᴛ ʀᴇɴᴀᴍɪɴɢ ᴄᴀᴩᴀᴄɪᴛy \n➢ /addpremium - ᴛᴏ ᴜᴩɢʀᴀᴅᴇ ᴜꜱᴇʀ ᴩʟᴀɴ**", quote=True, reply_markup=InlineKeyboardMarkup([
 		           [
-				   InlineKeyboardButton(" ꜱɪʟᴠᴇʀ", callback_data="vip1")
-				   ], [
-					InlineKeyboardButton(" ɢᴏʟᴅ", callback_data="vip2")
-				   ], [
-					InlineKeyboardButton(" ᴅɪᴀᴍᴏɴᴅ", callback_data="vip3")
+				   InlineKeyboardButton("• ᴄʟᴏꜱᴇ •", callback_data="cancel")
 					]]))
 
