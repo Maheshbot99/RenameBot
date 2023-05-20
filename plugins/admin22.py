021c58ca348f305e86d417468d03984a1dfa3f15
Maheshbot99 <115773959+Maheshbot99@users.noreply.github.com>
2023-05-20 08:46:02 +0530

    Update admin22.py

--- plugins/admin22.py
+++ plugins/admin22.py
@@ -5,19 +5,6 @@ from pyrogram import Client, filters
 from helper.date import add_date
 from helper.database import uploadlimit, usertype, addpre
 ADMIN = int(os.environ.get("ADMIN", 1484670284))
-log_channel = int(os.environ.get("LOG_CHANNEL", ""))
-
-
-@Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["warn"]))
-async def warn(c, m):
-        if len(m.command) >= 3:
-            try:
-                user_id = m.text.split(' ', 2)[1]
-                reason = m.text.split(' ', 2)[2]
-                await m.reply_text("User Notfied Sucessfully")
-                await c.send_message(chat_id=int(user_id), text=reason)
-            except:
-                 await m.reply_text("User Not Notfied Sucessfully 😔")
 
 
 @Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["addpremium"]))
