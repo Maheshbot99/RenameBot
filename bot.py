4a54609cea51cfb1fe458cfb4889734954de7bf5
Maheshbot99 <115773959+Maheshbot99@users.noreply.github.com>
2023-08-22 17:19:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,46 +1,40 @@
-from pyrogram import Client 
-from config import API_ID, API_HASH, BOT_TOKEN, CHANNEL, PORT
+import asyncio
+from pyrogram import Client, compose,idle
+import os
+
+from plugins.cb_data import app as Client2
 from aiohttp import web
-from route import web_server
 
-class Bot(Client):
+PORT = "8080"
 
-    def __init__(self):
-        super().__init__(
-            name="renamer",
-            api_id=API_ID,
-            api_hash=API_HASH,
-            bot_token=BOT_TOKEN,
-            workers=200,
-            plugins={"root": "plugins"},
-            sleep_threshold=15,
-        )
+TOKEN = os.environ.get("TOKEN", "")
 
-    async def start(self):
-        await super().start()
-        me = await self.get_me()
-        self.mention = me.mention
-        self.username = me.username 
-        self.force_channel = CHANNEL
-        if CHANNEL:
-            try:
-                link = await self.export_chat_invite_link(CHANNEL)                  
-                self.invitelink = link
-            except Exception as e:
-                print(e)
-                print("Make Sure Bot admin in force sub channel")             
-                self.force_channel = None
-        app = web.AppRunner(await web_server())
-        await app.setup()
-        bind_address = "0.0.0.0"       
-        await web.TCPSite(app, bind_address, PORT).start()     
-        print(f"{me.first_name} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳 ⚡️⚡️⚡️")
-      
+API_ID = int(os.environ.get("API_ID", ""))
 
-    async def stop(self, *args):
-        await super().stop()      
-        print("Bot Stopped")
-       
+API_HASH = os.environ.get("API_HASH", "")
 
-bot=Bot()
-bot.run()
+STRING = os.environ.get("STRING", "")
+
+bot = Client(
+
+           "Renamer",
+
+           bot_token=TOKEN,
+
+           api_id=API_ID,
+
+           api_hash=API_HASH,
+
+           plugins=dict(root='plugins'))
+
+
+if STRING:
+    apps = [Client2,bot]
+    for app in apps:
+        app.start()
+    idle()
+    for app in apps:
+        app.stop()
+
+else:
+    bot.run()
