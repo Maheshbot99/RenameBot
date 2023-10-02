118077df3593e9ccff7c2e9c31817641f1110c88
Maheshbot99 <115773959+Maheshbot99@users.noreply.github.com>
2023-10-02 09:05:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,8 @@
 import asyncio
 from pyrogram import Client, compose,idle
 import os
-from aiohttp import web
-from route import web_server
 
 from plugins.cb_data import app as Client2
-
-PORT = "8080"
 
 TOKEN = os.environ.get("TOKEN", "")
 
@@ -15,6 +11,8 @@ API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
 
 STRING = os.environ.get("STRING", "")
+
+
 
 bot = Client(
 
@@ -27,7 +25,7 @@ bot = Client(
            api_hash=API_HASH,
 
            plugins=dict(root='plugins'))
-
+           
 
 if STRING:
     apps = [Client2,bot]
@@ -36,6 +34,6 @@ if STRING:
     idle()
     for app in apps:
         app.stop()
-
+    
 else:
     bot.run()
